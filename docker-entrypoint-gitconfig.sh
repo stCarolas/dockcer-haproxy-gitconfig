b3040e6fba3a41b217aa622d961d3309147afd8c
stCarolas <stcarolas@gmail.com>
2018-05-26 12:37:52 +0300

    try to fix permisinos

--- docker-entrypoint-gitconfig.sh
+++ docker-entrypoint-gitconfig.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 /git-clone-config.sh
 echo  "args: $@"
+mkdir /usr/local/var || true
+mkdir /usr/local/var/lib || true
+mkdir /usr/local/var/lib/haproxy || true
+chmod -R 777 /usr/local/var/lib/haproxy
 /docker-entrypoint.sh "$@"
